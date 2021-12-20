e3f1b9093eab7d7155b93c3255a11ba7d8f1c912
v_ktlema <v_ktlema@tencent.com>
2021-12-20 20:58:33 +0800

    first commit

---
+++ src/main/java/com/mzl/incomeexpensemanagesystem/IncomeExpenseManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mzl.incomeexpensemanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeExpenseManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncomeExpenseManageSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mzl/incomeexpensemanagesystem/IncomeExpenseManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzl.incomeexpensemanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeExpenseManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
