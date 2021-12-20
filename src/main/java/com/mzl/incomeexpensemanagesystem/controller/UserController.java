5ae700de7ebf3f52e8e8852db88f405730e442b6
v_ktlema <v_ktlema@tencent.com>
2021-12-20 21:42:50 +0800

    h

---
+++ src/main/java/com/mzl/incomeexpensemanagesystem/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.mzl.incomeexpensemanagesystem.controller;
+
+/**
+ * @ClassName :   UserController
+ * @Description: TODO
+ * @Author: v_ktlema
+ * @CreateDate: 2021/12/20 21:41
+ * @Version: 1.0
+ */
+public class UserController {
+}
