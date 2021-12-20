2e5cfa4fb6c38fbb0480942932f47afb54b57c03
v_ktlema <v_ktlema@tencent.com>
2021-12-20 22:10:20 +0800

    firs

---
+++ src/main/java/com/mzl/incomeexpensemanagesystem/controller/NewsController.java
@@ -0,0 +1,11 @@
+package com.mzl.incomeexpensemanagesystem.controller;
+
+/**
+ * @ClassName :   NewsController
+ * @Description: TODO
+ * @Author: v_ktlema
+ * @CreateDate: 2021/12/20 22:09
+ * @Version: 1.0
+ */
+public class NewsController {
+}
